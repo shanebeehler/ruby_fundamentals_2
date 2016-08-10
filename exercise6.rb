3d60fef60e522228f298a7c6061b4806e3a81801
Shane <shanebeehler@me.com>
2016-08-10 09:51:40 -0400

    Created method to add item and puts list.

--- exercise6.rb
+++ exercise6.rb
@@ -1,3 +1,11 @@
 shopping_list = ["Apples", "Chips", "Pizza", "Ketchup"]
 
 shopping_list.each {|item| puts "* #{item}"}
+
+def groceries(new_item)
+  shopping_list = ["Apples", "Chips", "Pizza", "Ketchup"]
+  shopping_list << new_item
+  shopping_list.each {|item| puts "* #{item}"}
+end
+
+groceries("Rice")

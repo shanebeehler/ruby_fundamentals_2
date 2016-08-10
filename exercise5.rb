aa8e239d67bb962a1fcd2b06dd6fbf41322597fe
Shane <shanebeehler@me.com>
2016-08-10 09:19:53 -0400

    Convert user temp form F to C and puts result.

--- exercise5.rb
+++ exercise5.rb
@@ -1,3 +1,10 @@
 puts "Give me a temperature in Farenheit."
 
 temp = gets.chomp
+
+def far_cel(temp)
+  new_temp = (temp.to_i - 32) * 5/9
+  puts "#{temp} degrees Farenheit is #{new_temp} degrees Celcius."
+end
+
+far_cel(temp)
